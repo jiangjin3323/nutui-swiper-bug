cd1d5b066133692a78046507d99cb59610aad371
姜衿 <70458277@qq.com>
2023-04-26 17:05:53 +0800

    swiper组件BUG

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,42 +1,36 @@
-import {
-  Button,
-} from "@nutui/nutui-react-taro";
-import './index.scss'
-import { FC, useEffect } from 'react'
-import { useReady, useDidShow, useDidHide, usePullDownRefresh } from '@tarojs/taro'
-import { useSelector } from "react-redux";
-const Index:FC = ()=> {
-  // 可以使用所有的 React Hooks
-  useEffect(() => {})
+import React, { useState } from 'react'
+import { Swiper,SwiperItem } from '@nutui/nutui-react-taro';
 
-  // 对应 onReady
-  useReady(() => {})
-
-  // 对应 onShow
-  useDidShow(() => {})
-
-  // 对应 onHide
-  useDidHide(() => {})
-
-  // Taro 对所有小程序页面生命周期都实现了对应的自定义 React Hooks 进行支持
-  // 详情可查阅：【Hooks】
-  usePullDownRefresh(() => {})
-  //全局store
-  const store: any = useSelector((state) => state);
-
+const App = () => {
+  const [initPage1, setInitPage1] = useState(0)
+  const [height, setHeight] = useState<any>(150)
+  const onChange = (e) => {
+    // do something
+  }
   return (
-    <div className="nutui-react-demo">
-         <div className="index">
-          欢迎使用 NutUI React 开发 Taro 多端项目。
-          {store.userInfo}
-         </div>
-         <div className="index">
-           <Button type="primary" className="btn">
-             NutUI React Button
-           </Button>
-         </div>
-       </div>
+    <div className="demo-box" style={{ height: 150 }}>
+      <Swiper
+        height={height}
+        paginationColor="#426543"
+        autoPlay="3000"
+        initPage={initPage1}
+        paginationVisible
+        onChange={onChange}
+      >
+        <SwiperItem >
+          <img src="https://storage.360buyimg.com/jdc-article/NutUItaro34.jpg" alt="" />
+        </SwiperItem>
+        <SwiperItem >
+          <img src="https://storage.360buyimg.com/jdc-article/NutUItaro2.jpg" alt="" />
+        </SwiperItem>
+        <SwiperItem >
+          <img src="https://storage.360buyimg.com/jdc-article/welcomenutui.jpg" alt="" />
+        </SwiperItem>
+        <SwiperItem >
+          <img src="https://storage.360buyimg.com/jdc-article/fristfabu.jpg" alt="" />
+        </SwiperItem>
+      </Swiper>
+    </div>
   )
 }
-
-export default Index
+export default App;
